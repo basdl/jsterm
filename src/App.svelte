2d3b8f2b8bf4d3f148b085b2c06c0e6023fa08bc
Bastian Huber <1148027+basdl@users.noreply.github.com>
2023-11-24 20:45:07 +0100

    warning if the browser is not supporting webserial

--- src/App.svelte
+++ src/App.svelte
@@ -1,142 +1,31 @@
 <script lang="ts">
-  import "carbon-components-svelte/css/white.css";
   import {
-    Button,
-    Checkbox,
     Column,
     Grid,
+    InlineNotification,
     Row,
-    TextInput,
   } from "carbon-components-svelte";
-  import WordsView from "./WordsView.svelte";
-  import { IPortSettings, SPECIALBYTE_BREAK } from "./helper";
-  import PortSettings from "./port/PortSettings.svelte";
+  import MainApp from "./MainApp.svelte";
 
-  // Store
-  let bytes = [];
-  export let portSettings: IPortSettings = {
-    port: null,
-    baudRate: 9600,
-    dataBits: 8,
-    stopBits: 1,
-    parity: "none",
-    flowControl: "none",
-  };
-  let showAscii = true;
-  let showHex: boolean = true;
-  let showDecimal: boolean = true;
-  let newLineEveryNthCharacter: number = 0;
-  let newLineEveryNMsPause: number = 0;
-  let autoScroll: boolean = true;
-  let lastReceived = Date.now();
-
-  // Callbacks
-  async function onPortSelected() {
-    await portSettings.port.open(portSettings);
-
-    await readFromPort((bytes) => {
-      bytes.map((b) => newByteReceived(b));
-    });
-  }
-
-  function clearReceived() {
-    bytes = [];
-  }
-
-  function newByteReceived(b) {
-    const now = Date.now();
-    if (now - lastReceived > newLineEveryNMsPause && newLineEveryNMsPause > 0) {
-      bytes.push(SPECIALBYTE_BREAK);
-    }
-    lastReceived = now;
-    bytes.push(b);
-    bytes = bytes;
-  }
-
-  async function readFromPort(cb) {
-    while (portSettings.port.readable) {
-      const reader = portSettings.port.readable.getReader();
-      try {
-        while (true) {
-          const { value, done } = await reader.read();
-          if (done) {
-            // |reader| has been canceled.
-            break;
-          }
-
-          cb(value);
-        }
-      } catch (error) {
-        // Handle |error|...
-      } finally {
-        reader.releaseLock();
-      }
-    }
-  }
-
-  setInterval(() => {
-    //newByteReceived(Math.ceil(Math.random() * 255));
-  }, 100);
+  let serialPortAvailable = "serial" in navigator;
 </script>
 
-<div class="app">
-  <div class="sideBar">
-    <PortSettings
-      bind:settings={portSettings}
-      on:portSelected={onPortSelected}
-    />
-    <TextInput size="sm" labelText="Rx" disabled value={bytes.length} />
-
-    <Button size="small" on:click={clearReceived}>Clear received</Button>
-    <Checkbox labelText="Ascii" bind:checked={showAscii} />
-    <Checkbox labelText="Hex" bind:checked={showHex} />
-    <Checkbox labelText="Dec" bind:checked={showDecimal} />
-    <TextInput
-      size="sm"
-      labelText="Newline every ... char"
-      bind:value={newLineEveryNthCharacter}
-    />
-    <Checkbox labelText="Auto scroll" bind:checked={autoScroll} />
-    <TextInput
-      size="sm"
-      labelText="Newline after ... ms receive pause"
-      bind:value={newLineEveryNMsPause}
-    />
-
-    <a href="https://github.com/basdl/jsterm" class="githublink"
-      >Go to github project</a
-    >
-  </div>
-  <div class="wordsView">
-    <WordsView
-      {bytes}
-      {showAscii}
-      {showHex}
-      {showDecimal}
-      {newLineEveryNthCharacter}
-      {autoScroll}
-    />
-  </div>
-</div>
-
-<style>
-  .sideBar {
-    width: 20%;
-    padding: 2rem;
-  }
-
-  .app {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .wordsView {
-    width: 80%;
-    padding: 1rem;
-  }
-
-  .githublink {
-    position: fixed;
-    bottom: 2rem;
-  }
-</style>
+{#if serialPortAvailable}
+  <MainApp></MainApp>
+{:else}
+  <Grid>
+    <Row>
+      <Column></Column>
+      <Column>
+        <InlineNotification
+          hideCloseButton
+          kind="warning"
+          title="Missing browser support:"
+          subtitle="    Serial port is not available on this device / browser. Usually this is
+    possible on your PC."
+        /></Column
+      >
+      <Column></Column>
+    </Row>
+  </Grid>
+{/if}
